5cbec8febe5282a79b73f12bb6d95eaea52f8693
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-06-15 00:20:44 -0300

    includes new clientId for test auth in api spotify

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
     clientId: '7e9c96b1855e40aebaaf2de083649c14',
+    // clientId: '4d4e4816883045edb757e76354902278',
     redirectUrl: `${window.location.origin}/authorize`,
     webAPI: 'https://api.spotify.com/v1',
     scopes: [
